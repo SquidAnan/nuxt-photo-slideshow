6c60dd3c21be4b4eb6438f881bee1345d0c13464
SquidAnan <penguinandy0902@gmail.com>
2023-02-08 22:23:24 +0800

    add photo number

--- layouts/default.vue
+++ layouts/default.vue
@@ -6,13 +6,16 @@
       2023-02-05-NYCU
     </div>
     <div
-      class="grow-0 shrink-1 basis-auto flex items-center justify-center h-full min-h-0 m-12"
+      class="grow-0 shrink-1 basis-auto flex items-center justify-center h-full min-h-0 m-14 mt-0"
     >
-      <img
-        src="/images/IMG_4630.jpg"
-        alt="img"
-        class="max-h-full shadow-lg rounded"
-      />
+      <div class="relative flex items-center justify-center h-full rounded">
+        <img
+          src="/images/IMG_4630.jpg"
+          alt="img"
+          class="max-h-full shadow-lg"
+        />
+        <div class="absolute -right-10 bottom-0 text-base text-neutral-200">1/5</div>
+      </div>
     </div>
   </div>
 </template>
